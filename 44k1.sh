319deab81a215f0185a167e4500d0ff0e2d49c84
Jean Zundel <jzu@free.fr>
2011-04-09 12:12:12 +0200

    A small utility converting any WAV file to 44100 Hz mono

---
+++ 44k1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Normalizes any WAV file to a slampler-compatible format
+# (c) jzu@free.fr 2011
+
+if [ $# -eq 0 ] 
+then
+  echo Usage: `basename $0`  SRC.wav DST.wav 1>&2
+  exit 1
+fi
+
+sox $1 -c 1 -b 16 $2 rate -h 44100 dither -s
